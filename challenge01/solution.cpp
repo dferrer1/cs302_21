5ca11b4e50cf9af2df5eb75814d1d7487fffd2b1
Diego Ferrer <dferrer1@vols.utk.edu>
2021-08-22 13:53:11 -0400

    solution (while loop does not end)

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -1,6 +1,104 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
 // Challenge 01: Rotating Arrays; null solution
 
+/*
+
+Author: Diego Ferrer
+Dates: 8/19/21 - 8/27/21
+Collaborators:
+Description:
+
+*/
+
 int main(int argc, char *argv[]) {
-    return 0;
+	vector<int> values;
+	string inst, vals;
+	int val;
+	
+	int size;
+	int rotations;
+	char direction;
+
+	int fpos;
+	int k;
+
+	// begin loop
+	while (getline(cin, inst)) {
+	
+		// store input values
+		stringstream ss(inst);
+		ss >> size;
+		ss >> rotations;
+		ss >> direction;
+
+		// store values
+		getline(cin, vals);
+		stringstream vs(vals);
+		values.clear();
+		while(vs >> val) {
+			values.push_back(val);
+		}
+		
+		// calculate final position of '1'
+		fpos = rotations % size;
+		k = 0;
+
+		// left rotation
+		if (direction == 'L') {
+		
+			// print new array
+			for (int i=0; i < size; i++) {
+				if ((i+fpos) < size) {
+				
+					// print element
+					cout << values[i+fpos];
+
+					// increment counter
+					k++;
+				}
+				else {
+					// print element
+					cout << values[i-k];
+				}
+		
+				// print spaces
+				if (i < (size-1)) {
+					cout << ' ';
+				}
+			}
+		}
+
+		// right rotation
+		else if (direction == 'R') {
+			// print new array
+			for (int i=0; i < size; i++) {
+				if (i < fpos) {
+				
+					// print element
+					cout << values[i+size-fpos];
+
+					// increment counter
+					k++;
+				}
+				else {
+					// print element
+					cout << values[i-k];
+				}
+		
+				// print spaces
+				if (i < (size-1)) {
+					cout << ' ';
+				}
+			}
+			
+		}
+	}
+ 
+	return 0;
 }
 
