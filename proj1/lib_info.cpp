e08a8ae1119662f82528ce809a5115bda6f2e954
Diego Ferrer <dferrer1@vols.utk.edu>
2021-08-22 19:03:52 -0400

    initial commit

---
+++ proj1/lib_info.cpp
@@ -0,0 +1,33 @@
+/*
+Author: Diego Ferrer
+Dates: 8/22/21 - 8/30/21
+Collaborators: Caleb Hagens
+Description: This is the main file for the first project that I had to do for CS302. 
+			 The assignment involves taking a formatted text file in(using the command line to determine the name of the file),
+			 change the formatting, and print the info taken in with a set format.
+			 Ex:
+			 UNIX> lib_info Small.txt
+			 Coltrane, John: 4, 18:37
+					Giant Steps: 4, 18:37
+							1. Giant Steps: 4:46
+							3. Countdown: 2:25
+							6. Naima: 4:24
+							7. Mr. P.C.: 7:02
+			 Lyle, Bobby: 1, 5:16
+					Night Breeze: 1, 5:16
+							5. Naima: 5:16
+			 Puente, Tito: 1, 4:02
+					El Rey: 1, 4:02
+							5. Giant Steps: 4:02
+			 Tjader, Cal: 1, 5:36
+					A Fuego Vivo: 1, 5:36
+							6. Naima: 5:36
+			 Walton, Cedar: 4, 30:44
+					Eastern Rebellion: 1, 8:38
+							2. Naima: 8:38
+					Naima: 3, 22:06
+							2. This Guy's In Love With You: 8:10
+							4. Down In Brazil: 6:07
+							6. Naima: 7:49
+*/
+#include "mixtape_utils.h"
