05db321bebe4ab01239cdd82a0efa254f63360cb
Diego Ferrer <dferrer1@vols.utk.edu>
2021-09-13 12:27:20 -0400

    dummy code implemented

--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -1,6 +1,26 @@
 // Challenge 06: Palindrome Permutation
+// Author: Diego Ferrer
+// Dates: 9/13 - 9/
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// is_palindrome prototype
+bool is_palindrome(string s);
 
 int main(int argc, char *argv[]) {
-  return (0);
-}
+	vector<string> isv;
+	vector<string> pv;
+	// // Read in input
+	// // Check if input strings are palindromes
+	// // Output all palindromes
+	return (0);
+} // end of main
 
+// is_palindrome:	takes in a string, outputs a bool
+//					checks if the input string is a palindrome
+bool is_palindrome(string s) {
+	return false;
+} // end of is_palindrome
