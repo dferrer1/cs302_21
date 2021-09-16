6134deab99116d87cdcbabf1c5e405c43961dd67
Diego Ferrer <dferrer1@vols.utk.edu>
2021-09-15 23:21:34 -0400

    counts all characters, still needs to exclude non alphabetical characters

--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -1,9 +1,10 @@
 // Challenge 06: Palindrome Permutation
 // Author: Diego Ferrer
-// Dates: 9/13 - 9/
+// Dates: 9/13 - 9/17
 
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
 #include <string>
 #include <sstream>
 
@@ -13,22 +14,24 @@ using namespace std;
 void is_palindrome(string s);
 
 int main(int argc, char *argv[]) {
-	unordered_set<string> ius;
+	//unordered_set<string> ius;
 	string input;
 	// // Read in input
 	// read all phrases from standard input, store them in unordered_set
     while (getline(cin, input)) {
-		ius.insert(input);
+		//ius.insert(input);
+		is_palindrome(input);
 	}
 	
+	/*
 	// access each key and check if its a palindrome
 	unordered_set<string>::iterator iusi;
 	for (iusi = ius.begin(); iusi != ius.end(); iusi++) {
-		is_palindrome((*iusi));
+		//is_palindrome((*iusi));
 		//DEBUG: Check input unordered_set
 		cout << (*iusi) << endl;
 	}
-	
+	*/
 
 	return (0);
 } // end of main
@@ -37,7 +40,32 @@ int main(int argc, char *argv[]) {
 //					checks if the input string is a palindrome
 //					couts result
 void is_palindrome(string s) {
+	unordered_map<char, int> charc;
+	//unordered_map<char, int>::iterator charcit;
+	//const auto& charcit;
+	bool palindrome = false;
+
 	// // Check if input strings are palindromes
+	// count each character instance in s
+	for (int i = 0; i < int(s.size()); i++) {
+		if (charc.find(s[i]) == charc.end()) {
+			charc.insert({s[i], 1});
+		} else {
+			charc.at(s[i])++;
+		}
+	}
+
+	//DEBUG CHUNK
+	cout << s << " values: \n	";
+	for (const auto& charcit : charc) {
+		cout << charcit.first << " " << charcit.second << ", ";
+	}
+	cout << endl;
+
 	// // Output all results
-	
+	if (palindrome) {
+		//cout << "\"" << s << "\" is a palindrome permutation" << endl;
+	} else {
+		//cout << "\"" << s << "\" is not a palindrome permutation" << endl;
+	}
 } // end of is_palindrome
