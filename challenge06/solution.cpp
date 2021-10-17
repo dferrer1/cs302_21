2107d47fb71481dba1642b23e2a97620278a6d7a
Diego Ferrer <dferrer1@vols.utk.edu>
2021-10-17 17:55:08 -0400

    challenge 6

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -1,4 +1,8 @@
+// Author: Diego Ferrer
 // Challenge 7: Minimum Spanning Tree
+// Overview: This challenge was to take in the number of nodes and an adjacency graph that had the weights of paths. To accomplish this, I ran a 
+//			 read in the paths from input and used Prim's algorithm to go through the graph, taking the smallest weighted path to nodes that 
+//			 hadn't been added to the MST
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -8,47 +12,17 @@
 using namespace std;
 
 // Main Execution
-
-struct Node {
-	int row = 0;
-	int col = 0;
-	int backedge = 0;
-	int dist = -1;
-	bool visited = false;
-};
-
-struct Graph {
-	vector<Node> graph;
-	map<char, int> edge_weights;
-	int n;
-};
-
-
 int main(int argc, char *argv[]) {
-
 	vector<vector<int>> ag; // double vector to store edge weights
-	//map<int,int> aMST; // final MST; update as you go through the multimap
-	//vector<pair<int,int>> aMST;
-	multiset<pair<char,char>> apath; // used to prevent duplicate paths
 	multimap<int, pair<int,int>> aedges; // used for Prim's
-	
-
-	//vector<Node> graph;
 	multimap<int,int> edges;
 	vector<int> dm;
 	string nvertices, row;
 	int nvert;
-	//int distance;
 	int gnum = 0;
 
-	// read in nvertices
-	// while(getline(cin, nvertices)) {
-	
 	while(cin >> nvert)	{
 		map<int,set<int>> aMST; // final MST; update as you go through the multimap
-		//vector<pair<int,int>> aMST;
-		// DEBUG: loop check
-		//cout << "LOOP: " << gnum << endl;
 
 		// output formatting
 		if (gnum != 0) {
@@ -65,44 +39,16 @@ int main(int argc, char *argv[]) {
 			// read in each distance value
 			for (int j = 0; j < nvert; j++) {
 				cin >> ag[i][j];
-				//cin >> distance;
-				//v[i][j] = distance;
 			}
 		}
 
 		//// Use Prim's Algorithm
 		// add the first node's edges to aedges
-		//aedges.insert({ag[0][0],{0, 0}});
 		for (size_t j = 0; j < ag[0].size(); j++) {
 			if (ag[0][j] != -1) {
-				//DEBUG: check edge adds !!!
-				//printf("initial node adding {%d:{0, %d}} to aedges \n", ag[0][j], j);
-
 				aedges.insert({ag[0][j],{0, j}});
 			}
 		}
-
-		// add the first node's smallest adjacent edge to the MST
-		//aMST.insert({aedges.begin()->second.first, aedges.begin()->second.second});
-		//aMST.insert({0,0});
-
-		//DEBUG: check values
-		//printf("aedges.begin->second.second: %d\n", aedges.begin()->second.second);
-		
-		/*
-		// add the edges of the second node
-		for (size_t j = 0; j < ag[aedges.begin()->second.second].size(); j++) {
-			if (ag[aedges.begin()->second.second][j] != -1) {
-				//DEBUG: check edge adds
-				printf("second node adding {%d:{%d, %d}} to aedges \n", ag[aedges.begin()->second.second][j], j);
-
-				aedges.insert({ag[aedges.begin()->second.second][j],{aedges.begin()->second.second, j}});
-			}
-		}
-		*/
-
-		// remove the first edge after adding it
-		//aedges.erase(aedges.begin());
 
 		int total_weight = 0;
 		int weight;
@@ -115,9 +61,6 @@ int main(int argc, char *argv[]) {
 
 		// go through each edge
 		while(aedges.size() != 0) {
-			//DEBUG: check values !!!
-			//printf("aedges.begin()->second.first: %d\n", aedges.begin()->second.first);
-			//printf("aedges.begin()->second.second: %d\n", aedges.begin()->second.second);
 
 			weight = aedges.begin()->first;
 			cnode = aedges.begin()->second.first;
@@ -128,17 +71,12 @@ int main(int argc, char *argv[]) {
 			
 			// check that the current node isn't visited
 			if (visited.find(nnode) == visited.end()) {
-			// if it isn't visited
-				//DEBUG: check MST adds !!!
-				//printf("adding {%d, %d} to MST \n", cnode, nnode);
-				
 				// add the smallest edge adjacent to the current node to the MST in the right order
 				if (cnode < nnode) {
 					if (aMST.find(cnode) == aMST.end()) {
 						set<int> nset;
 						nset.insert(nnode);
 						aMST.insert({cnode, nset});
-						//printf("inserting new node: %d\n",cnode);
 					} else {
 						aMST.at(cnode).insert(nnode);
 					}
@@ -147,12 +85,10 @@ int main(int argc, char *argv[]) {
 						set<int> nset;
 						nset.insert(cnode);
 						aMST.insert({nnode, nset});
-						//printf("inserting new node: %d\n",nnode);
 					} else {
 						aMST.at(nnode).insert(cnode);
 					}
 				}
-				//aMST.push_back({cnode, nnode});
 
 				// add the weight to total weight
 				total_weight += weight;
@@ -164,76 +100,24 @@ int main(int argc, char *argv[]) {
 				for (size_t j = 0; j < ag[nnode].size(); j++) {
 					
 					if (ag[nnode][j] != -1) {
-						//DEBUG: check edge adds !!!
-						//printf("adding {%d:{%d, %d}} to aedges \n", ag[nnode][j], nnode, j);
-						
 						aedges.insert({ag[nnode][j],{nnode, j}});
 					}
 				}
 			}
 		}
 
-		//DEBUG: check all edges added to MST !!!
-		//for (size_t i = 0; i < aMST.size(); i++) {
-			//printf("First Node: %d; Second Node: %d\n", aMST[i].first, aMST[i].second);
-		//}
-
 		//// output
 		// print the total weight
 		printf("%d\n", total_weight);
 		
 		// print each path
-		for (int i = 0; i <= aMST.size(); i++) {
-		//for (map<int,set<int>>::iterator msti = aMST.begin(); msti != aMST.end(); msti++) {
+		for (size_t i = 0; i <= aMST.size(); i++) {
 			if (aMST.find(i) != aMST.end()) {
 				for (set<int>::iterator si = aMST.at(i).begin(); si != aMST.at(i).end(); si++) {
 					printf("%c%c\n", char(i+65), char((*si)+65));
 				}
 			}
-		//}
 		}
-		/*
-		for (size_t i = 0; i < aMST.size(); i++) {
-			printf("%c%c\n", char(aMST[i].first+65), char(aMST[i].second+65));
-		}
-		*/
-
-		/*
-		//DEBUG: check all edges added to MST
-		for (map<int,int>::iterator msti = aMST.begin(); msti != aMST.end(); msti++) {
-			printf("First Node: %d; Second Node: %d\n", msti->first, msti->second);
-		}
-		*/
-
-		/*
-		//// read in distance matrix
-		// read in each row
-		for(int i = 0; i < stoi(nvertices); i++) {
-			getline(cin, row);
-			stringstream rss(row);
-
-			// read in each column value and store it in the distance matrix
-			for (int j = 0; j < stoi(nvertices); j++) {
-				rss >> distance;
-				
-				//DEBUG: check distance
-				//cout << distance;
-
-				dm.push_back(distance);
-			}
-
-			//DEBUG: output formatting
-			//cout << endl;
-		}
-
-		//DEBUG: check input
-		for (int i = 0; i < stoi(nvertices); i++) {
-			for (int j = 0; j < stoi(nvertices); j++) {
-				cout << dm[i*stoi(nvertices) + j] << " ";
-			}
-			cout << endl;
-		}
-		*/
 		
 		// output formatting
 		gnum++;
